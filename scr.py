e824a1bc9555f6e37e0bd6cff293d95c85df11ed
Adam <lastsicarius@protonmail.com>
2017-09-15 14:54:00 +0200

    Fouth commit- remove trash code

--- scr.py
+++ scr.py
@@ -1,22 +1,18 @@
+#importowanie potrzebnych modulow
 from flask import Flask
-from redis import Redis, RedisError
+import os
 import string
 import random
-import os
-import socket
-
-
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
+from random import randint
+#konfiguracja Flska v2
 app = Flask(__name__)
-
 @app.route("/")
+#losowanie i wyswietlanie hasla
 def hello():
-
-
     html ="Haslo: <b>: {haslo} "
-    return html.format(haslo=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(6)))
-
+    N=randint(6,12)
+    haslo=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(N))
+    return html.format(haslo=haslo)
+#konfiguracja Flska v3
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
